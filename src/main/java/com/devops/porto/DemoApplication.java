2097b9faee2b57b0fa71a4d3a2986abf8258b2c3
Miguel Fonseca <miguel.cnf@gmail.com>
2017-01-25 19:31:43 +0000

    add info

--- src/main/java/com/devops/porto/DemoApplication.java
+++ src/main/java/com/devops/porto/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.devops.porto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,3 +14,11 @@ public class DemoApplication {
     }
 }
 
+@Component
+class MyInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("Description", "My super app that says Hi!");
+    }
+}
